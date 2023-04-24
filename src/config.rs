a5a765b1b17ac89ecda6ffeed2ab0653b1fae1f6
oiwn <alex@imscraping.ninja>
2023-04-24 23:41:07 +0700

    basic traits

--- src/config.rs
+++ src/config.rs
@@ -1,54 +1,140 @@
 use serde_yaml::Value;
-use std::{fs, io, path};
+use std::{
+    fs,
+    io::{self, BufRead},
+    path,
+};
 
-pub trait Config {
-    fn name(&self) -> &String;
+pub trait Configurable {
+    fn name(&self) -> &str;
     fn config(&self) -> &Value;
 
     // read configuration from yaml config
-    fn from_config(
+    fn load_config(
         config_file_path: impl AsRef<path::Path>,
     ) -> Result<serde_yaml::Value, io::Error> {
         let content: String = fs::read_to_string(config_file_path)?;
-        let config: serde_yaml::Value = serde_yaml::from_str(&content)?;
+        let config: serde_yaml::Value = serde_yaml::from_str(&content).unwrap();
         Ok(config)
     }
 
     // support methods
-    fn load_user_agents();
+    fn load_text_file_lines(
+        file_path: impl AsRef<path::Path>,
+    ) -> Result<Vec<String>, io::Error> {
+        let file = fs::File::open(file_path)?;
+        let lines: Vec<String> = io::BufReader::new(file)
+            .lines()
+            .map(|l| l.expect("Could not parse line"))
+            .collect();
 
-    /// Extract Value from config using dot notation i.e. "mongodb.default"
+        Ok(lines)
+    }
+
+    /// Extract Value from config using dot notation i.e. "app.concurrency"
     fn get_config_value(&self, key: &str) -> Option<&Value> {
         let keys: Vec<&str> = key.split('.').collect();
-        get_value_recursive(self.config(), &keys)
+        Self::get_value_recursive(self.config(), &keys)
+    }
+
+    fn get_value_recursive<'a>(
+        config: &'a Value,
+        keys: &[&str],
+    ) -> Option<&'a Value> {
+        if keys.is_empty() {
+            return None;
+        };
+
+        match config {
+            serde_yaml::Value::Mapping(map) => {
+                let key = keys[0];
+                let remaining_keys = &keys[1..];
+
+                if let Some(value) =
+                    map.get(&serde_yaml::Value::String(key.to_string()))
+                {
+                    if remaining_keys.is_empty() {
+                        Some(value)
+                    } else {
+                        Self::get_value_recursive(value, remaining_keys)
+                    }
+                } else {
+                    None
+                }
+            }
+            _ => None,
+        }
     }
 }
 
-pub fn get_value_recursive<'a>(
-    config: &'a Value,
-    keys: &[&str],
-) -> Option<&'a Value> {
-    if keys.is_empty() {
-        return None;
-    };
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    match config {
-        serde_yaml::Value::Mapping(map) => {
-            let key = keys[0];
-            let remaining_keys = &keys[1..];
+    pub struct Application {
+        name: String,
+        config: Value,
+        user_agents: Option<Vec<String>>,
+    }
 
-            if let Some(value) =
-                map.get(&serde_yaml::Value::String(key.to_string()))
-            {
-                if remaining_keys.is_empty() {
-                    Some(value)
-                } else {
-                    get_value_recursive(value, remaining_keys)
-                }
-            } else {
-                None
+    impl Configurable for Application {
+        fn name(&self) -> &str {
+            self.name.as_str()
+        }
+        fn config(&self) -> &Value {
+            &self.config
+        }
+    }
+
+    impl Application {
+        fn from_config(config_file_path: impl AsRef<path::Path>) -> Self {
+            let config = Self::load_config(config_file_path);
+            Self {
+                name: "test-app".to_string(),
+                config: config.unwrap(),
+                user_agents: None,
             }
         }
-        _ => None,
+
+        fn load_uas(&mut self, uas_file_path: impl AsRef<path::Path>) {
+            self.user_agents = Self::load_text_file_lines(uas_file_path).ok();
+        }
+    }
+
+    #[test]
+    fn test_load_config() {
+        let config_path = "tests/simple_config.yml";
+        let app = Application::from_config(config_path);
+
+        assert_eq!(app.config["app"]["threads"].as_u64(), Some(4));
+        assert_eq!(app.config()["app"]["max_queue"].as_u64(), Some(500));
+        assert_eq!(app.user_agents, None);
+    }
+
+    #[test]
+    fn test_get_config_value() {
+        let config_path = "tests/simple_config.yml";
+        let app = Application::from_config(config_path);
+
+        assert_eq!(
+            app.get_config_value("logging.log_to_redis")
+                .unwrap()
+                .as_bool(),
+            Some(true)
+        )
+    }
+
+    #[test]
+    fn test_load_lines() {
+        let config_path = "tests/simple_config.yml";
+        let mut app = Application::from_config(config_path);
+        let uas_file_path = {
+            app.get_config_value("app.user_agents_file")
+                .unwrap()
+                .as_str()
+                .unwrap()
+                .to_owned()
+        };
+        app.load_uas(&uas_file_path);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,79 +1,12 @@
-use std::io::{self, BufRead};
-use std::{fs, path};
+pub mod config;
 
-mod config;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
 
-#[derive(thiserror::Error, Debug)]
-pub enum ApplicationError {
-    #[error("Cannot open/read config file")]
-    ReadConfigFile(#[source] io::Error),
-
-    #[error("Cannot parse the configuration file")]
-    ParseConfig(#[source] serde_yaml::Error),
-
-    #[error("Can not open user agents file")]
-    ReadUserAgentsFile(#[source] io::Error),
-}
-
-pub struct Application {
-    pub name: String,
-    pub config: serde_yaml::Value,
-}
-
-impl Application {
-    pub fn new(
-        name: &str,
-        yaml_config_path: impl AsRef<path::Path>,
-    ) -> Result<Self, ApplicationError> {
-        let config = Application::load_config(yaml_config_path)?;
-
-        Ok(Application {
-            name: name.to_string(),
-            config,
-        })
-    }
-
-    pub fn from_env(name: &str) {}
-
-    fn load_config(
-        config_file_path: impl AsRef<path::Path>,
-    ) -> Result<serde_yaml::Value, ApplicationError> {
-        let content: String = fs::read_to_string(config_file_path)
-            .map_err(ApplicationError::ReadConfigFile)?;
-        let config: serde_yaml::Value = serde_yaml::from_str(&content)
-            .map_err(ApplicationError::ParseConfig)?;
-        Ok(config)
-    }
-
-    fn load_text_file_into_lines(
-        ua_file_path: impl AsRef<path::Path>,
-    ) -> Result<Vec<String>, ApplicationError> {
-        let file =
-            fs::File::open(ua_file_path).map_err(ApplicationError::ReadTextFile)?;
-        let lines = io::BufReader::new(file).lines();
-        let user_agents: Vec<String> =
-            lines.map(|l| l.expect("Could not parse line")).collect();
-
-        Ok(user_agents)
-    }
-
-    pub fn get_config_value(&self, key: &str) -> Option<&serde_yaml::Value> {
-        let keys: Vec<&str> = key.split('.').collect();
-        get_value_recursive(&self.config, &keys)
-    }
-}
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
